674e35ee18480a066ad669f3f1c6b351cf82c03a
Kong Mun Jun <43808355+GnokRacso@users.noreply.github.com>
2020-06-11 22:42:54 +0800

    Add files via upload

---
+++ Variables.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testing : Monobehavior {
+
+    void Start (){
+        /*
+        Variables for decimals:
+        -Float (float)  = 32-bit number
+        -Double (double)= 64-bit number (more precise, 更多小数点)
+
+        Variable for whole number:
+        -Integer (int) = output 出来的number 不会有小数点
+            eg，
+            int num = 123;
+        */
+        int a = 1;
+        float b = 2.4f; //must have 'f' behind the number, number can without decimals
+        double c = 2.5d; /*can without d, 不用吃饱没事做就放一个*/
+
+        /*
+        Variable for Sentences:
+        -String (string) = character based variables
+            eg，
+        */
+        string name = "between the quotes";
+        print(name);
+        
+    }
+
+    void Update(){
+
+    }
+}
